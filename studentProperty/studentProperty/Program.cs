23a386ebccafc4d6d464e38ff7d48eca74cbb7f3
mahmoud-eltablawy62 <134718324+mahmoud-eltablawy62@users.noreply.github.com>
2023-07-13 13:10:32 +0300

    tasks_day5

---
+++ studentProperty/studentProperty/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace student_data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            studentData studentData = new studentData();
+            Console.WriteLine("enter the Ssn:-");
+            string ssn = Console.ReadLine();
+            Console.WriteLine("enter the name:-");
+            string name = Console.ReadLine();
+            Console.WriteLine("enter the age:-");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the Address:-");
+            string address = Console.ReadLine();
+
+            studentData.Intail(ssn, name, age, address);
+            Console.WriteLine(studentData.Print());
+
+        }
+    }
+    class studentData
+    {
+        string name;
+        string ssn;
+        int age;
+        string Address;
+       
+        public void Intail(string Name ,string Ssn , int Age , string _Address )
+        {
+            name = Name;    
+            ssn = Ssn;
+            age = Age;
+            Address = _Address;
+        }
+
+        public string Print()
+        {
+            return $"====================\nname==>{name} \nSsn==>{ssn} \nage==>{age} \nAddress==>{Address} ";
+        }
+
+    }
+}
